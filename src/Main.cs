0846b273ebaf104f7060a4557e4781851260f1fc
octo <allblvnk@gmail.com>
2020-10-23 20:05:32 +0100

    Figure out how to get melon pref values

--- src/Main.cs
+++ src/Main.cs
@@ -6,6 +6,20 @@ using MelonLoader;
 
 public class ModSettings : MelonMod
 {
+    public override void OnLevelWasInitialized(int level)
+    {
+        if (level != 2) return;
+        var prefs = MelonPrefs.GetPreferences();
+
+        foreach (var category in prefs)
+        {
+            MelonLogger.Log("Found mod: " + category.Key);
+            foreach (var pref in category.Value)
+            {
+                MelonLogger.Log("Found pref:" + pref.Key);
+            }
+        }
+    }
 
 }
 
