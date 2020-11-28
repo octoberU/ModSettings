8e4dafaa351d78f7811f18569a103d638c18da4b
octo <allblvnk@gmail.com>
2020-11-28 13:18:43 +0000

    Removed debug method

--- src/Main.cs
+++ src/Main.cs
@@ -6,20 +6,6 @@ using MelonLoader;
 
 public class ModSettings : MelonMod
 {
-    public override void OnLevelWasInitialized(int level)
-    {
-        if (level != 3) return;
-        var prefs = MelonPrefs.GetPreferences();
-
-        foreach (var category in prefs)
-        {
-            MelonLogger.Log("Found mod: " + category.Key);
-            foreach (var pref in category.Value)
-            {
-                MelonLogger.Log("Found pref:" + pref.Key);
-            }
-        }
-    }
 
 }
 
