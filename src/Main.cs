97901cd3952c5686316dfe75e8ac9ea131ecf4bb
octo <allblvnk@gmail.com>
2020-11-28 16:33:06 +0000

    Version bump

--- src/Main.cs
+++ src/Main.cs
@@ -2,7 +2,7 @@ using MelonLoader;
 
 
 [assembly: MelonGame("Harmonix Music Systems, Inc.", "Audica")]
-[assembly: MelonInfo(typeof(ModSettings), "Mod Settings", "0.1.0", "octo", "https://github.com/octoberU/ModSettings")]
+[assembly: MelonInfo(typeof(ModSettings), "Mod Settings", "0.1.1", "octo", "https://github.com/octoberU/ModSettings")]
 
 public class ModSettings : MelonMod
 {
